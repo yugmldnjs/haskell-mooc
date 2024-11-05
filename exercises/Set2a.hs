c45c12ef0d698bab060029b02155be570f7cf37d
Heewon Yu <gmldnjs0317@gmail.com>
2024-11-05 20:34:29 +0900

    solve Set2a Ex6

--- exercises/Set2a.hs
+++ exercises/Set2a.hs
@@ -90,7 +90,9 @@ isPalindrome str = str == reverse str
 --   palindromify "abracacabra" ==> "acaca"
 
 palindromify :: String -> String
-palindromify s = todo
+palindromify s
+  | isPalindrome s = s
+  | otherwise      = palindromify (init (tail s))
 
 ------------------------------------------------------------------------------
 -- Ex 7: implement safe integer division, that is, a function that
