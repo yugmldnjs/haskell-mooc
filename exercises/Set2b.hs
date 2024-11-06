6e2d05d7c998ca3f69ad0d15b2c2acac1d25ac35
Heewon Yu <gmldnjs0317@gmail.com>
2024-11-06 12:23:23 +0900

    solve Set2b Ex5

--- exercises/Set2b.hs
+++ exercises/Set2b.hs
@@ -103,7 +103,10 @@ leftpad s n
 -- * you'll probably need a recursive helper function
 
 countdown :: Integer -> String
-countdown = todo
+countdown n = "Ready! " ++ countdown' n
+
+countdown' 0 = "Liftoff!"
+countdown' n = show n ++ "... " ++ countdown' (n-1)
 
 ------------------------------------------------------------------------------
 -- Ex 6: implement the function smallestDivisor that returns the
