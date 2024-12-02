902865994e0a72291e8ca730f7965b41d25833b0
Heewon Yu <gmldnjs0317@gmail.com>
2024-12-02 14:57:07 +0900

    solve Set3a Ex3

--- exercises/Set3a.hs
+++ exercises/Set3a.hs
@@ -56,7 +56,9 @@ mapMaybe f (Just x) = Just (f x)
 --   mapMaybe2 div (Just 6) Nothing   ==>  Nothing
 
 mapMaybe2 :: (a -> b -> c) -> Maybe a -> Maybe b -> Maybe c
-mapMaybe2 f x y = todo
+mapMaybe2 f _ Nothing = Nothing
+mapMaybe2 f Nothing _ = Nothing
+mapMaybe2 f (Just x) (Just y) = Just (f x y) 
 
 ------------------------------------------------------------------------------
 -- Ex 4: define the functions firstHalf and palindrome so that
