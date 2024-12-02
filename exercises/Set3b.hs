4647d75d61029c165197ed7826a923043eff8d95
Heewon Yu <gmldnjs0317@gmail.com>
2024-12-02 22:44:53 +0900

    solve Set3b Ex3

--- exercises/Set3b.hs
+++ exercises/Set3b.hs
@@ -67,7 +67,9 @@ sums' i a b = if a > i then []
 --   mylast 0 [1,2,3] ==> 3
 
 mylast :: a -> [a] -> a
-mylast def xs = todo
+mylast def [] = def
+mylast def [x] = x
+mylast def (x:xs) = mylast def xs 
 
 ------------------------------------------------------------------------------
 -- Ex 4: safe list indexing. Define a function indexDefault so that
